10160f2d350a7e9a5814d048dcdf14a4281e5fd9
Jenish Chauhan <69527455+cJen2@users.noreply.github.com>
2020-09-22 20:40:47 +0530

    Update run.py

--- 3D-GAN/run.py
+++ 3D-GAN/run.py
@@ -253,14 +253,14 @@ if __name__ == '__main__':
                 dis_losses.append(d_loss)
 
                 # Every 10th mini-batch, generate volumes and save them
-                if index % 10 == 0:
-                    z_sample2 = np.random.normal(0, 0.33, size=[batch_size, 1, 1, 1, z_size]).astype(np.float32)
-                    generated_volumes = generator.predict(z_sample2, verbose=3)
-                    for i, generated_volume in enumerate(generated_volumes[:5]):
-                        voxels = np.squeeze(generated_volume)
-                        voxels[voxels < 0.5] = 0.
-                        voxels[voxels >= 0.5] = 1.
-                        saveFromVoxels(voxels, "results/img_{}_{}_{}".format(epoch, index, i))
+            if epoch % 10 == 0:
+                z_sample2 = np.random.normal(0, 0.33, size=[batch_size, 1, 1, 1, z_size]).astype(np.float32)
+                generated_volumes = generator.predict(z_sample2, verbose=3)
+                for i, generated_volume in enumerate(generated_volumes[:1]):
+                    voxels = np.squeeze(generated_volume)
+                    voxels[voxels < 0.5] = 0.
+                    voxels[voxels >= 0.5] = 1.
+                    saveFromVoxels(voxels, "results/img_{}_{}".format(epoch, i))
 
             # Write losses to Tensorboard
             write_log(tensorboard, 'g_loss', np.mean(gen_losses), epoch)
